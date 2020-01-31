d83f3de5aa4ffc93c3669a711eee269bbd29c71f
omeinsotelo <omeinsotelo@gmail.com>
2020-01-31 16:35:12 -0500

    task 0,.h and print functions

---
+++ 0-bubble_sort.c
@@ -0,0 +1,35 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - function that sorts an array of integers in ascending order
+ * @array: poiter to array.
+ * @size: size of the array.
+ * Return: none.
+*/
+
+void bubble_sort(int *array, size_t size)
+{
+    size_t lim = size, cnt = 0;
+    int tmp = 0;
+
+    if (array == NULL)
+    {
+        return;
+    }
+    while (lim > 0)
+    {
+        while (cnt + 1 < lim)
+        {
+            if (array[cnt] > array[cnt + 1])
+            {
+                tmp = array[cnt];
+                array[cnt] = array[cnt + 1];
+                array[cnt + 1] = tmp;
+                print_array(array, size);
+            }
+            cnt++;
+        }
+        lim--;
+        cnt = 0;
+    }
+}

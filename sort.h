99fc24cab2b725f96e4497496d54fbbc4c4a47eb
omeinsotelo <omeinsotelo@gmail.com>
2020-02-04 15:20:26 -0500

    sort.h

--- sort.h
+++ sort.h
@@ -31,5 +31,5 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-
+void swapAr(int *i, int *j);
 #endif /* SORTING_ALGORITHMS */

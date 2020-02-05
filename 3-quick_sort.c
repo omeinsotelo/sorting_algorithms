c39d1d34b04601b4f08cf3467438ad8ee19cecfe
omeinsotelo <omeinsotelo@gmail.com>
2020-02-05 16:47:10 -0500

    fixing outputs

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -6,57 +6,41 @@
  */
 void quick_sort(int *array, size_t size)
 {
-	unsigned int i, j, pivot, low, high, swap;
+	quick_sort_recurcion(array, size, 0, size - 1);
+}
+/**
+ * quick_sort_recurcion - sorts an array using recurcion
+ * @array: int
+ * @size: size_t
+ * @inicial: inicial of array
+ * @final: end of array
+ */
+void quick_sort_recurcion(int *array, size_t size, int inicial, int final)
+{
+	int ext, temp, piv, inte = inicial - 1;
 
-	if (size <= 1 || array == NULL)
+	if (inicial >= final)
+	{
 		return;
+	}
 
-	pivot = high = size - 1;
-	i = j = low = 0;
-	do {
-		swap = 0;
-		j = i = low;
-		pivot = high;
-		for (; j < pivot;)
+	piv = array[final];
+
+	for (ext = inicial; ext <= final; ext++)
+	{
+		if (array[ext] <= piv)
 		{
-			if (array[j] > array[pivot])
-				j++;
-			else
+			inte++;
+			temp = array[inte];
+			array[inte] = array[ext];
+			array[ext] = temp;
+			if (ext != inte)
 			{
-				swapAr(&array[i], &array[j]);
-				j++;
-				i++;
-				if (array[j] != array[i])
-				{
-					swap = 1;
-					print_array(array, size);
-				}
+				print_array(array, size);
 			}
 		}
-		if (array[i] > array[pivot])
-		{
-			swapAr(&array[i], &array[j]);
-			swap = 1;
-			print_array(array, size);
-		}
-		else
-			high--;
-		if (swap == 1)
-			low++;
-	} while (low < high);
-}
+	}
 
-/**
- * swapAr - sorts an array using the Quick sort
- * @i: int
- * @j: int
- */
-
-void swapAr(int *i, int *j)
-{
-	int temp;
-
-	temp = *j;
-	*j = *i;
-	*i = temp;
+	quick_sort_recurcion(array, size, inicial, inte - 1);
+	quick_sort_recurcion(array, size, inte + 1, final);
 }
--- sort.h
+++ sort.h
@@ -32,4 +32,5 @@ void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void swapAr(int *i, int *j);
+void quick_sort_recurcion(int *array, size_t size, int inicial, int final);
 #endif /* SORTING_ALGORITHMS */
